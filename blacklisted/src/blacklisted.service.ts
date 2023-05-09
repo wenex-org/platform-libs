e7f1cc6b53f0a7882e12ec97a011135f7ba3f68d
Vahid V <vhid.vz@gmail.com>
2023-05-09 18:52:57 +0330

    update

--- blacklisted/src/blacklisted.service.ts
+++ blacklisted/src/blacklisted.service.ts
@@ -7,29 +7,21 @@ import { BLACKLISTED_PREFIX_KEY } from './consts';
 export class BlacklistedService {
   constructor(private readonly redisService: RedisService) {}
 
-  public async isBlacklisted(
-    prefixes: string[],
-    ...str: string[]
-  ): Promise<boolean> {
-    const keys = str
-      .map((s) => prefixes.map((p) => `${BLACKLISTED_PREFIX_KEY}:${p}:${s}`))
-      .flat();
+  public async isBlacklisted(...keys: string[]): Promise<boolean> {
+    keys = keys.map((k) => `${BLACKLISTED_PREFIX_KEY}:${k}`);
     return !!(await this.redisService.exists(...keys));
   }
 
   public async put(
     str: string,
-    options?: { prefix?: string; ttl?: number },
+    { prefix, ttl }: { prefix: string; ttl: number },
   ): Promise<'OK'> {
-    return this.redisService.set(
-      `${BLACKLISTED_PREFIX_KEY}:${(options?.prefix ?? ':') + str}`,
-      'true',
-      'EX',
-      options?.ttl,
-    );
+    const key = `${BLACKLISTED_PREFIX_KEY}:${prefix}:${str}`;
+    return this.redisService.set(key, 'true', 'EX', ttl);
   }
 
-  public async del(str: string, prefix?: string): Promise<number> {
-    return this.redisService.del(`${BLACKLISTED_PREFIX_KEY}:${prefix}:${str}`);
+  public async del(str: string, prefix: string): Promise<number> {
+    const key = `${BLACKLISTED_PREFIX_KEY}:${prefix}:${str}`;
+    return this.redisService.del(key);
   }
 }
