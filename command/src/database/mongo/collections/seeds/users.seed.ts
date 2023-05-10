8229427d4e07b980cb0d4c4d9a5b375dbdafec75
Vahid V <vhid.vz@gmail.com>
2023-05-11 00:41:03 +0330

    update

--- command/src/database/mongo/collections/seeds/users.seed.ts
+++ command/src/database/mongo/collections/seeds/users.seed.ts
@@ -8,12 +8,14 @@ import {
 } from '@app/common/consts';
 import { CreateUserDto } from '@app/common/dto';
 import { MongoId } from '@app/common/utils';
+import { Status } from '@app/common/enums';
 
 export const users: CreateUserDto[] = [
   {
     _id: MongoId(SYSTEM_UID),
     owner: SYSTEM_UID,
     clients: [CID],
+    status: Status.Active,
     username: SYSTEM_USERNAME,
     password: SYSTEM_PASSWORD,
     subjects: [SYSTEM_SUBJECT],
