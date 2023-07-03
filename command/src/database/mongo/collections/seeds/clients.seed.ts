cdbca218d687d8560289b9283a28cee6f46088ca
Vahid V <vhid.vz@gmail.com>
2023-07-03 15:16:10 +0330

    update

--- command/src/database/mongo/collections/seeds/clients.seed.ts
+++ command/src/database/mongo/collections/seeds/clients.seed.ts
@@ -5,6 +5,7 @@ import {
   CLIENT_NAME,
   CLIENT_SECRET,
   CLIENT_TTL,
+  EXPIRATION_DATE,
   SYSTEM_DOMAIN,
   SYSTEM_UID,
 } from '@app/common/consts';
@@ -19,9 +20,6 @@ export const clients: CreateClientDto[] = [
     name: CLIENT_NAME,
     plan: Plan.Platinum,
     clients: [CID],
-    // repl: get(ClientsService).generateApiKey('6448d4122ed1fc913e4d4a5a', ['localhost', '127.0.0.1', '::1', '::ffff:127.0.0.1'], new Date('2099-01-01T00:00:00.000Z'))
-    api_key:
-      'U2FsdGVkX1//gsTlqKSvuBbx1BG+yCBbDzctf4Cx6XRtNZJdNhGdlJk00oW0CeafCh7Z+MZIXXzW8ANdqkUz9gFs0kmMAw9oaB3pr97hX3e7k0nPJ3cLUF1Ia3+hzH0lHLN7GRcVheL7CblRbsC1fe+L+IgMstj4zOGjv/hnlys41qh9Ej79/tIOmxX2WU3Wl011JJV5igVICXoA4A60k5z4E2Iel8KwyQj1t/KBxpWfsAoigYuA5jBGk6KNuWObQTW5UNdT4Ap0W139I/64F/D0HonqjR/Jq4Yc2PRvxMcsU9y+aTq01HmkOyTAdHEmGFieYPXPTQMstmZHvu9cWlh1bcC2xu5tW2ALk+0ez+SQJouxq0Ai4fMyk3awBMP1',
     client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
     state: State.Approved,
@@ -38,7 +36,7 @@ export const clients: CreateClientDto[] = [
       GrantType.ClientCredential,
       GrantType.AuthorizationCode,
     ],
-    expiration_date: new Date('2099-01-01T00:00:00.000Z'),
+    expiration_date: EXPIRATION_DATE,
     created_in: APP_ID ?? CID,
     created_by: SYSTEM_UID ?? CID,
     created_at: new Date(),
